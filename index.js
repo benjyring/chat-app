1b5875d116297b1bdeade00fa171650e40689297
Ben Jyring <benjyring@gmail.com>
2017-12-07 07:24:45 -0600

    Correct syntax errors

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-var app = require('express')();
+var express = require('express');        // call express
+var app = express();                 // define our app using express
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 var path = require('path');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(require('express').static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
+  console.log('a user connected');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  }).on('disconnect', function(){
+    console.log('a user disconnected')
   });
 });
 
